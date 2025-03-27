d7bba0cca489b8dabdb395a693db008f080d58e2
Moyosore Akinola <moyokolade26@gmail.com>
2025-03-27 14:29:58 -0600

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -229,7 +229,7 @@ elif page == "Make Prediction":
 # streamlit_app.py
 
 import streamlit as st
-from streamlit-gsheets import GSheetsConnection
+from streamlit_gsheets import GSheetsConnection
 
 
 # Create a connection object.
